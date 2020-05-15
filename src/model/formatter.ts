c1d19bba96b70823e8f72b27aeb9803e8eb31c37
pomodorozhong <pomodorozhong@gmail.com>
2020-05-16 01:19:34 +0800

    feat✨(Model.Formatter): implement formatWithoutBr()

--- src/model/formatter.ts
+++ src/model/formatter.ts
@@ -23,4 +23,24 @@ export class Formatter implements IFormatter {
 
         return out;
     }
+
+    formatWithoutBr(
+        type: string,
+        scope: string,
+        subject: string,
+        body: string,
+        footer: string
+    ): string { 
+        let out: string = "";
+
+        out += type;
+        if (scope != "") {
+            out += "(" + scope + ")";
+        }
+        out += ": " + subject;
+        out += body == "" ? "" : "\n\n" + body;
+        out += footer == "" ? "" : "\n\n" + footer;
+
+        return out;
+    };
 }
