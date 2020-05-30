1de8c401ba0e3177f066fe1643a7e58d7027f8e4
pomodorozhong <pomodorozhong@gmail.com>
2020-05-30 22:29:36 +0800

    feat✨(Model.IFormatter): add baseFormat()

--- src/interface/model/formatter.interface.ts
+++ src/interface/model/formatter.interface.ts
@@ -1,18 +1,22 @@
 export interface IFormatter {
-    format(
-        type: string,
-        scope: string,
-        subject: string,
-        body: string,
-        footer: string
-    ): string;
+  // stringArr =[type, scope, subject, body, footer]
+  baseFormat(
+    stringArr: [string, string, string, string, string]
+  ): [string, string, string, string, string];
 
+  format(
+    type: string,
+    scope: string,
+    subject: string,
+    body: string,
+    footer: string
+  ): string;
 
-    formatWithoutBr(
-        type: string,
-        scope: string,
-        subject: string,
-        body: string,
-        footer: string
-    ): string;
+  formatWithoutBr(
+    type: string,
+    scope: string,
+    subject: string,
+    body: string,
+    footer: string
+  ): string;
 }
