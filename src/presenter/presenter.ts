daf2a3a64fcb1c76d9f6bae4f5e5891d067d0164
pomodorozhong <pomodorozhong@gmail.com>
2020-06-27 00:21:49 +0800

    chore⚙️(presenter): prettier the presenter.ts

--- src/presenter/presenter.ts
+++ src/presenter/presenter.ts
@@ -1,44 +1,46 @@
 import { IPresenter } from "../interface/presenter/presenter.interface";
 import { IView } from "../interface/view/view.interface";
-import { View } from "../view/view"
+import { View } from "../view/view";
 import { IFormatter } from "../interface/model/formatter.interface";
 import { Formatter } from "../model/formatter";
 
 export class Presenter implements IPresenter {
-    private view: IView;
-    private formatter: IFormatter;
+  private view: IView;
+  private formatter: IFormatter;
 
-    constructor() {
-        this.view = new View(document, this);
-        this.formatter = new Formatter();
+  constructor() {
+    this.view = new View(document, this);
+    this.formatter = new Formatter();
 
-        this.initialize();
-    }
+    this.initialize();
+  }
 
-    // Sync View and Model
-    initialize(): void {
+  // Sync View and Model
+  initialize(): void {}
 
-    }
+  toFormat(): void {
+    this.view.displayFormattedText(
+      this.formatter.format(
+        this.view.getType(),
+        this.view.getScope(),
+        this.view.getSubject(),
+        this.view.getBody(),
+        this.view.getFooter()
+      )
+    );
+  }
 
-    toFormat(): void {
-        this.view.displayFormattedText(
-            this.formatter.format(
-                this.view.getType(),
-                this.view.getScope(),
-                this.view.getSubject(),
-                this.view.getBody(),
-                this.view.getFooter()));
-    }
+  toFormatWithoutBr(): void {
+    this.view.copyTextToClipboard(
+      this.formatter.formatWithoutBr(
+        this.view.getType(),
+        this.view.getScope(),
+        this.view.getSubject(),
+        this.view.getBody(),
+        this.view.getFooter()
+      )
+    );
+  }
 
-    toFormatWithoutBr(): void {
-        this.view.copyTextToClipboard(
-            this.formatter.formatWithoutBr(
-                this.view.getType(),
-                this.view.getScope(),
-                this.view.getSubject(),
-                this.view.getBody(),
-                this.view.getFooter()));
-    }
-
-    toCheckRule(): void { }
+  toCheckRule(): void {}
 }
