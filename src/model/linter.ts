3bd5cfa03272bdf16e42035c3253334406e7af8d
pomodorozhong <pomodorozhong@gmail.com>
2020-06-27 01:16:30 +0800

    feat✨(linter): implement lint()

--- src/model/linter.ts
+++ src/model/linter.ts
@@ -5,11 +5,26 @@ export class Linter implements ILinter {
 
   // stringArr =[type, scope, subject, body, footer]
   lint(stringArr: [string, string, string, string, string]) {
-    throw new Error("Not Implemented.");
+    let warning: string = "";
+    let [type, scope, subject, body, footer] = stringArr;
+
+    if (!this.IsTextLowerCaseCheck(subject)) {
+      warning += "subject 行首字母不需要大寫。<br>";
+    }
+    if (!this.IsTextLenLimitCheck(subject, 50)) {
+      warning += "subject 太長了，超過 50 個字元。<br>";
+    }
+
+    // removing last <br>
+    if (warning.length > 0) {
+      warning = warning.slice(0, -4);
+    }
+
+    return warning;
   }
 
   IsTextLowerCaseCheck(text: string): boolean {
-    return text == text.toLowerCase();
+    return text[0] == text[0].toLowerCase();
   }
 
   IsTextLenLimitCheck(text: string, LimitLen: number): boolean {
