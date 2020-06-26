f7d8eb8cbc34943f1c9bfa069930d49ff36038ff
pomodorozhong <pomodorozhong@gmail.com>
2020-06-27 00:53:48 +0800

    chore⚙️(Model.linter): prettier the linter.ts

--- src/model/linter.ts
+++ src/model/linter.ts
@@ -1,19 +1,18 @@
 import { ILinter } from "../interface/model/linter.interface";
 
 export class Linter implements ILinter {
-    constructor() {
-    }
+  constructor() {}
 
-    // stringArr =[type, scope, subject, body, footer]
-    lint(stringArr: [string, string, string, string, string]) {
-        throw new Error("Not Implemented.");
-    }
+  // stringArr =[type, scope, subject, body, footer]
+  lint(stringArr: [string, string, string, string, string]) {
+    throw new Error("Not Implemented.");
+  }
 
-    IsTextLowerCaseCheck(text: string): boolean {
-        return (text == text.toLowerCase());
-    }
+  IsTextLowerCaseCheck(text: string): boolean {
+    return text == text.toLowerCase();
+  }
 
-    IsTextLenLimitCheck(text: string, LimitLen: number): boolean {
-        return (text.length < LimitLen);
-    }
+  IsTextLenLimitCheck(text: string, LimitLen: number): boolean {
+    return text.length < LimitLen;
+  }
 }
