58746bcbcb263cddcb133ab715a2979311968c4a
pomodorozhong <31407195+pomodorozhong@users.noreply.github.com>
2020-10-04 21:27:15 +0800

    feat✨: add clear button for every input box (#24)
    * style💄(view): code formatting
    
    * feat✨(html, css, view): add clear button for every input box
    
    * style💄(linter): code formatting
    
    * fix🐛(linter): handle empty string in `IsTextLowerCaseCheck`
    
    * feat✨: hide clear button automatically
    
    Close #24

--- src/ts/model/linter.ts
+++ src/ts/model/linter.ts
@@ -3,42 +3,48 @@ import { Formatter } from "./formatter";
 import { IFormatter } from "../interface/model/formatter.interface";
 
 export class Linter implements ILinter {
-  constructor() {}
+    constructor() {}
 
-  // stringArr =[type, scope, subject, body, footer]
-  lint(stringArr: [string, string, string, string, string]) {
-    let warning: string = "";
-    let [type, scope, subject, body, footer] = stringArr;
+    // stringArr =[type, scope, subject, body, footer]
+    lint(stringArr: [string, string, string, string, string]) {
+        let warning: string = "";
+        let [type, scope, subject, body, footer] = stringArr;
 
-    let formatter: IFormatter = new Formatter();
-    [type, scope, subject, body, footer] = formatter.baseFormat([
-      type,
-      scope,
-      subject,
-      body,
-      footer,
-    ]);
+        let formatter: IFormatter = new Formatter();
+        [type, scope, subject, body, footer] = formatter.baseFormat([
+            type,
+            scope,
+            subject,
+            body,
+            footer,
+        ]);
 
-    if (!this.IsTextLowerCaseCheck(subject)) {
-      warning += "subject 行首字母不需要大寫。<br>";
+        if (!this.IsTextLowerCaseCheck(subject)) {
+            warning += "subject 行首字母不需要大寫。<br>";
+        }
+        if (!this.IsTextLenLimitCheck(subject, 50)) {
+            warning += "subject 太長了，超過 50 個字元。<br>";
+        }
+
+        // removing last <br>
+        if (warning.length > 0) {
+            warning = warning.slice(0, -4);
+        }
+
+        return warning;
     }
-    if (!this.IsTextLenLimitCheck(subject, 50)) {
-      warning += "subject 太長了，超過 50 個字元。<br>";
+
+    IsTextLowerCaseCheck(text: string | undefined): boolean {
+        let isValid = false;
+        if (text == undefined || text.length == 0) {
+            isValid = true;
+        } else {
+            isValid = text[0] == text[0].toLowerCase();
+        }
+        return isValid;
     }
 
-    // removing last <br>
-    if (warning.length > 0) {
-      warning = warning.slice(0, -4);
+    IsTextLenLimitCheck(text: string, LimitLen: number): boolean {
+        return text.length < LimitLen;
     }
-
-    return warning;
-  }
-
-  IsTextLowerCaseCheck(text: string): boolean {
-    return text[0] == text[0].toLowerCase();
-  }
-
-  IsTextLenLimitCheck(text: string, LimitLen: number): boolean {
-    return text.length < LimitLen;
-  }
 }
--- src/ts/view/view.ts
+++ src/ts/view/view.ts
@@ -3,169 +3,230 @@ import { IPresenter } from "../interface/presenter/presenter.interface";
 import { Presenter } from "../presenter/presenter";
 
 export class View implements IView {
-  DOM: Document;
-  presenter: IPresenter;
-  constructor(DOM: Document, presenter: Presenter) {
-    this.presenter = presenter;
-    this.DOM = DOM;
-    this.DOM.getElementById("ddl_type").addEventListener("change", userInputed);
-    this.DOM.getElementById("txt_scope").addEventListener("input", userInputed);
-    this.DOM.getElementById("txt_subject").addEventListener(
-      "input",
-      userInputed
-    );
-    this.DOM.getElementById("txa_body").addEventListener("input", userInputed);
-    this.DOM.getElementById("txa_footer").addEventListener(
-      "input",
-      userInputed
-    );
+    DOM: Document;
+    presenter: IPresenter;
+    constructor(DOM: Document, presenter: Presenter) {
+        this.presenter = presenter;
+        this.DOM = DOM;
+        this.DOM.getElementById("ddl_type").addEventListener("change", userInputed);
+        this.DOM.getElementById("txt_scope").addEventListener("input", userInputed);
+        this.DOM.getElementById("txt_subject").addEventListener("input", userInputed);
+        this.DOM.getElementById("txa_body").addEventListener("input", userInputed);
+        this.DOM.getElementById("txa_footer").addEventListener("input", userInputed);
 
-    let self = this;
-    this.DOM.getElementById("btn_copy").addEventListener("click", btnClicked);
+        let self = this;
+        this.DOM.getElementById("btn_copy").addEventListener("click", btnClicked);
 
-    // Event Handler
-    function userInputed() {
-      self.presenter.toFormat();
-      self.presenter.toCheckRule();
-    }
-    function btnClicked() {
-      self.presenter.toFormatWithoutBr();
-    }
+        // Event Handler
+        function userInputed() {
+            self.presenter.toFormat();
+            self.presenter.toCheckRule();
+            self.autoSetClearButtonVisibility();
+        }
+        function btnClicked() {
+            self.presenter.toFormatWithoutBr();
+        }
 
-    this.setPlaceholder();
-    this.setTypes();
-  }
-
-  setTypes(): void {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("ddl_type")
-    );
-
-    const types = this.presenter.getTypes();
-
-    types.forEach((t) => {
-      var opt = document.createElement("option");
-      opt.appendChild(document.createTextNode(t));
-      input.appendChild(opt);
-    });
-  }
-
-  setPlaceholder(): void {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("ddl_type")
-    );
-    var opt = document.createElement("option");
-    opt.disabled = true;
-    opt.selected = true;
-    opt.appendChild(document.createTextNode("<type>"));
-    input.appendChild(opt);
-
-    input = <HTMLInputElement>this.DOM.getElementById("txt_scope");
-    input.placeholder = "<scope>";
-
-    input = <HTMLInputElement>this.DOM.getElementById("txt_subject");
-    input.placeholder = "<subject>";
-
-    input = <HTMLInputElement>this.DOM.getElementById("txa_body");
-    input.placeholder = "<body>";
-
-    input = <HTMLInputElement>this.DOM.getElementById("txa_footer");
-    input.placeholder = "<footer>";
-  }
-
-  // DOM Manipulation
-  displayFormattedText(text: string): void {
-    this.DOM.getElementById("p_formatted").innerHTML = text;
-  }
-
-  displayWarning(text: string): void {
-    if (text === "") {
-      this.toggleWarningVisibility(true);
-    } else {
-      this.toggleWarningVisibility(false);
-      this.DOM.getElementById("p_warning").innerHTML = text;
-    }
-  }
-
-  toggleWarningVisibility(isHidden: boolean): void {
-    let element: HTMLElement = this.DOM.getElementById("p_warning");
-
-    if (isHidden) {
-      element.style.visibility = "hidden";
-      element.style.position = "absolute";
-    } else {
-      element.style.visibility = "visible";
-      element.style.position = "relative";
-    }
-  }
-
-  // DOM Accessing
-  getType(): string {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("ddl_type")
-    );
-    return input.value;
-  }
-  getScope(): string {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("txt_scope")
-    );
-    return input.value;
-  }
-  getSubject(): string {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("txt_subject")
-    );
-    return input.value;
-  }
-  getBody(): string {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("txa_body")
-    );
-    return input.value;
-  }
-  getFooter(): string {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      this.DOM.getElementById("txa_footer")
-    );
-    return input.value;
-  }
-
-  fallbackCopyTextToClipboard(text) {
-    var textArea = document.createElement("textarea");
-    textArea.value = text;
-
-    // Avoid scrolling to bottom
-    textArea.style.top = "0";
-    textArea.style.left = "0";
-    textArea.style.position = "fixed";
-
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-
-    try {
-      var successful = document.execCommand("copy");
-      var msg = successful ? "successful" : "unsuccessful";
-      console.log("Fallback: Copying text command was " + msg);
-    } catch (err) {
-      console.error("Fallback: Oops, unable to copy", err);
+        this.setPlaceholder();
+        this.setTypes();
+        this.setClearButton(userInputed);
     }
 
-    document.body.removeChild(textArea);
-  }
-  copyTextToClipboard(text) {
-    if (!navigator.clipboard) {
-      this.fallbackCopyTextToClipboard(text);
-      return;
+    setTypes(): void {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("ddl_type");
+
+        const types = this.presenter.getTypes();
+
+        types.forEach((t) => {
+            var opt = document.createElement("option");
+            opt.appendChild(document.createTextNode(t));
+            input.appendChild(opt);
+        });
     }
-    navigator.clipboard.writeText(text).then(
-      function () {
-        console.log("Async: Copying to clipboard was successful!");
-      },
-      function (err) {
-        console.error("Async: Could not copy text: ", err);
-      }
-    );
-  }
+
+    setPlaceholder(): void {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("ddl_type");
+        var opt = document.createElement("option");
+        opt.disabled = true;
+        opt.selected = true;
+        opt.appendChild(document.createTextNode("<type>"));
+        input.appendChild(opt);
+
+        input = <HTMLInputElement>this.DOM.getElementById("txt_scope");
+        input.placeholder = "<scope>";
+
+        input = <HTMLInputElement>this.DOM.getElementById("txt_subject");
+        input.placeholder = "<subject>";
+
+        input = <HTMLInputElement>this.DOM.getElementById("txa_body");
+        input.placeholder = "<body>";
+
+        input = <HTMLInputElement>this.DOM.getElementById("txa_footer");
+        input.placeholder = "<footer>";
+    }
+
+    setClearButton(userInputed: Function): void {
+        let targetIds: Array<string> = ["txt_scope", "txt_subject", "txa_body", "txa_footer"];
+        let ids: Array<string> = [
+            "btn_clearScope",
+            "btn_clearSubject",
+            "btn_clearBody",
+            "btn_clearFooter",
+        ];
+        let containers: Array<string> = ["container2", "container3", "container4", "container5"];
+        let clears: Array<EventListener> = [clearScope, clearSubject, clearBody, clearFooter];
+
+        let btn = document.createElement("input");
+        btn.type = "button";
+        btn.value = "x";
+        btn.style.visibility = "hidden";
+
+        let self = this;
+
+        let btnClone1 = <HTMLInputElement>btn.cloneNode(true);
+        btnClone1.id = ids[0];
+        btnClone1.addEventListener("click", clears[0]);
+        let container1 = this.DOM.getElementById(containers[0]);
+        container1?.appendChild(btnClone1);
+        function clearScope() {
+            let element: HTMLTextAreaElement = self.DOM.getElementById(targetIds[0]);
+            element.value = "";
+            userInputed();
+        }
+
+        let btnClone2 = <HTMLInputElement>btn.cloneNode(true);
+        btnClone2.id = ids[1];
+        btnClone2.addEventListener("click", clears[1]);
+        let container2 = this.DOM.getElementById(containers[1]);
+        container2?.appendChild(btnClone2);
+        function clearSubject() {
+            let element: HTMLTextAreaElement = self.DOM.getElementById(targetIds[1]);
+            element.value = "";
+            userInputed();
+        }
+
+        let btnClone3 = <HTMLInputElement>btn.cloneNode(true);
+        btnClone3.id = ids[2];
+        btnClone3.addEventListener("click", clears[2]);
+        let container3 = this.DOM.getElementById(containers[2]);
+        container3?.appendChild(btnClone3);
+        function clearBody() {
+            let element: HTMLTextAreaElement = self.DOM.getElementById(targetIds[2]);
+            element.value = "";
+            userInputed();
+        }
+
+        let btnClone4 = <HTMLInputElement>btn.cloneNode(true);
+        btnClone4.id = ids[3];
+        btnClone4.addEventListener("click", clears[3]);
+        let container4 = this.DOM.getElementById(containers[3]);
+        container4?.appendChild(btnClone4);
+        function clearFooter() {
+            let element: HTMLTextAreaElement = self.DOM.getElementById(targetIds[3]);
+            element.value = "";
+            userInputed();
+        }
+    }
+
+    // DOM Manipulation
+    displayFormattedText(text: string): void {
+        this.DOM.getElementById("p_formatted").innerHTML = text;
+    }
+
+    displayWarning(text: string): void {
+        if (text === "") {
+            this.toggleWarningVisibility(true);
+        } else {
+            this.toggleWarningVisibility(false);
+            this.DOM.getElementById("p_warning").innerHTML = text;
+        }
+    }
+
+    toggleWarningVisibility(isHidden: boolean): void {
+        let element: HTMLElement = this.DOM.getElementById("p_warning");
+
+        if (isHidden) {
+            element.style.visibility = "hidden";
+            element.style.position = "absolute";
+        } else {
+            element.style.visibility = "visible";
+            element.style.position = "relative";
+        }
+    }
+
+    autoSetClearButtonVisibility(): void {
+        let targetIds: Array<string> = ["txt_scope", "txt_subject", "txa_body", "txa_footer"];
+        let buttonIds: Array<string> = [
+            "btn_clearScope",
+            "btn_clearSubject",
+            "btn_clearBody",
+            "btn_clearFooter",
+        ];
+
+        for (let i = 0; i < targetIds.length; i++) {
+            const value = (<HTMLTextAreaElement>this.DOM.getElementById(targetIds[i])).value;
+            let element = <HTMLTextAreaElement>this.DOM.getElementById(buttonIds[i]);
+            element.style.visibility = value.length > 0 ? "visible" : "hidden";
+        }
+    }
+
+    // DOM Accessing
+    getType(): string {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("ddl_type");
+        return input.value;
+    }
+    getScope(): string {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("txt_scope");
+        return input.value;
+    }
+    getSubject(): string {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("txt_subject");
+        return input.value;
+    }
+    getBody(): string {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("txa_body");
+        return input.value;
+    }
+    getFooter(): string {
+        let input: HTMLInputElement = <HTMLInputElement>this.DOM.getElementById("txa_footer");
+        return input.value;
+    }
+
+    fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand("copy");
+            var msg = successful ? "successful" : "unsuccessful";
+            console.log("Fallback: Copying text command was " + msg);
+        } catch (err) {
+            console.error("Fallback: Oops, unable to copy", err);
+        }
+
+        document.body.removeChild(textArea);
+    }
+    copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            this.fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(
+            function () {
+                console.log("Async: Copying to clipboard was successful!");
+            },
+            function (err) {
+                console.error("Async: Could not copy text: ", err);
+            }
+        );
+    }
 }
