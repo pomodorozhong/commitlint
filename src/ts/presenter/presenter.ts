8d26e5f3768f1e972b869413085847dfa1e9ef6a
pomodorozhong <pomodorozhong@gmail.com>
2020-11-15 00:56:15 +0800

    style💄(presenter): code formatting

--- src/ts/presenter/presenter.ts
+++ src/ts/presenter/presenter.ts
@@ -8,58 +8,58 @@ import { Linter } from "../model/linter";
 import { getTypesFromConfig } from "../model/getTypesFromConfig";
 
 export class Presenter implements IPresenter {
-  private view: IView;
-  private formatter: IFormatter;
-  private linter: ILinter;
+    private view: IView;
+    private formatter: IFormatter;
+    private linter: ILinter;
 
-  constructor() {
-    this.view = new View(document, this);
-    this.formatter = new Formatter();
-    this.linter = new Linter();
+    constructor() {
+        this.view = new View(document, this);
+        this.formatter = new Formatter();
+        this.linter = new Linter();
 
-    this.initialize();
-  }
+        this.initialize();
+    }
 
-  // Sync View and Model
-  initialize(): void {}
+    // Sync View and Model
+    initialize(): void {}
 
-  toFormat(): void {
-    this.view.displayFormattedText(
-      this.formatter.format(
-        this.view.getType(),
-        this.view.getScope(),
-        this.view.getSubject(),
-        this.view.getBody(),
-        this.view.getFooter()
-      )
-    );
-  }
+    toFormat(): void {
+        this.view.displayFormattedText(
+            this.formatter.format(
+                this.view.getType(),
+                this.view.getScope(),
+                this.view.getSubject(),
+                this.view.getBody(),
+                this.view.getFooter()
+            )
+        );
+    }
 
-  toFormatWithoutBr(): void {
-    this.view.copyTextToClipboard(
-      this.formatter.formatWithoutBr(
-        this.view.getType(),
-        this.view.getScope(),
-        this.view.getSubject(),
-        this.view.getBody(),
-        this.view.getFooter()
-      )
-    );
-  }
+    toFormatWithoutBr(): void {
+        this.view.copyTextToClipboard(
+            this.formatter.formatWithoutBr(
+                this.view.getType(),
+                this.view.getScope(),
+                this.view.getSubject(),
+                this.view.getBody(),
+                this.view.getFooter()
+            )
+        );
+    }
 
-  toCheckRule(): void {
-    this.view.displayWarning(
-      this.linter.lint([
-        this.view.getType(),
-        this.view.getScope(),
-        this.view.getSubject(),
-        this.view.getBody(),
-        this.view.getFooter(),
-      ])
-    );
-  }
+    toCheckRule(): void {
+        this.view.displayWarning(
+            this.linter.lint([
+                this.view.getType(),
+                this.view.getScope(),
+                this.view.getSubject(),
+                this.view.getBody(),
+                this.view.getFooter(),
+            ])
+        );
+    }
 
-  getTypes(): Array<string> {
-    return getTypesFromConfig();
-  }
+    getTypes(): Array<string> {
+        return getTypesFromConfig();
+    }
 }
